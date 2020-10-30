595f6a7dce06c0860526f67b8fabf09c5f259b7c
Evabas <51789607+Evabas@users.noreply.github.com>
2020-10-30 15:01:53 +0100

    Add files via upload

---
+++ build/precache-manifest.bd5e88b33557dec3402f94ee9ccb0cf5.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff551460f61afd77b508fff6c7704540",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffa2a2d2816aba56d40e",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "133a43b62ec9b7ec3ce6",
+    "url": "/static/css/main.ee8a63de.chunk.css"
+  },
+  {
+    "revision": "ffa2a2d2816aba56d40e",
+    "url": "/static/js/2.87eca0f2.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.87eca0f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "133a43b62ec9b7ec3ce6",
+    "url": "/static/js/main.8f96be26.chunk.js"
+  },
+  {
+    "revision": "df4c2066a89920c4c12d",
+    "url": "/static/js/runtime-main.25f99785.js"
+  },
+  {
+    "revision": "b6adeca39781bc3e4307b1d0b3258b08",
+    "url": "/static/media/ashtanga.b6adeca3.png"
+  },
+  {
+    "revision": "770d5e9b62e8677d05c7ccb109771f6c",
+    "url": "/static/media/bikram.770d5e9b.png"
+  },
+  {
+    "revision": "d9232aef753366180122620de099e45f",
+    "url": "/static/media/fondTapis1.d9232aef.jpg"
+  },
+  {
+    "revision": "e6dad88962c6960f34833fe4cdf994df",
+    "url": "/static/media/hatha.e6dad889.jpg"
+  },
+  {
+    "revision": "231de00be12558a6ed49435c47022ca2",
+    "url": "/static/media/instagram-logo-white.231de00b.png"
+  },
+  {
+    "revision": "307ae9f991a207491801e0a4608a8e23",
+    "url": "/static/media/iyengar.307ae9f9.png"
+  },
+  {
+    "revision": "de8248ccd17d01c366fcb11c6c726de6",
+    "url": "/static/media/kundalini.de8248cc.png"
+  },
+  {
+    "revision": "1a280d77bd25c4fe1d4bcfc94a5b4796",
+    "url": "/static/media/logo.1a280d77.png"
+  },
+  {
+    "revision": "e8542f41d36ab8412fbbb87d8294e6c3",
+    "url": "/static/media/portrait3.e8542f41.jpg"
+  },
+  {
+    "revision": "8182619a55fde556324653b98d6e6334",
+    "url": "/static/media/portrait5.8182619a.jpg"
+  },
+  {
+    "revision": "747817f1944fc7ff35df1582e4432a70",
+    "url": "/static/media/studio2.747817f1.jpg"
+  },
+  {
+    "revision": "c2be62bbfae78575059299a4ea8b5fa1",
+    "url": "/static/media/tapis1.c2be62bb.jpg"
+  },
+  {
+    "revision": "ed9865e8e4f5ee01aa36c5e7ed9f8711",
+    "url": "/static/media/tapis2.ed9865e8.jpg"
+  },
+  {
+    "revision": "ad8d5006150c320558da1617fbfb75fa",
+    "url": "/static/media/tapis3.ad8d5006.jpg"
+  },
+  {
+    "revision": "3709429d7aa8972caf0d545eee08c7a8",
+    "url": "/static/media/tapisMain.3709429d.jpg"
+  },
+  {
+    "revision": "68459e45a55bb0e870f451d545fca49b",
+    "url": "/static/media/vinyasa.68459e45.png"
+  }
+]);
