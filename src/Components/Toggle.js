fef185b244fbbed4643cca26ac2095a34624d300
Evabas <51789607+Evabas@users.noreply.github.com>
2020-11-12 14:59:16 +0100

    Add files via upload

--- src/Components/Toggle.js
+++ src/Components/Toggle.js
@@ -1,35 +1,41 @@
 import React, { useState } from 'react'  
 import '../App.css';
-import {ButtonGroup,Button, Row, Col} from 'react-bootstrap';
+import {Jumbotron, ButtonGroup, Button, Row, Col, Container} from 'react-bootstrap';
 import YogaDoux from './YogaDoux.js'
 import YogaDyn from './YogaDyn.js'
-import Cours from './Lessons.js'
+import Lessons from './Lessons.js'
 
 const Toggle = () => {
 const yogas = ["Ensemble des cours","Yoga Doux", "Yoga Dynamique"];
 const [myYoga, setMyYoga] = useState ('')
 
-return (
-<Row>
-  <Col className=" col-12 text-center group">
-  <ButtonGroup aria-label="groupe de boutons" size="lg">
-
-        {yogas.map(yoga => (
-          <Button variant="secondary"
-          className="px-5 m-5"
-          key={yoga}
-          onClick={()=> setMyYoga(yoga)}
+  return (
+    <Container fluid >
+      <Row>
+        <Col className="text-center group">
+          <Jumbotron style={{backgroundColor : "rgb(192, 217, 194)"}}>
+          <ButtonGroup aria-label="groupe de boutons" size="lg" className="cours" 
           >
-            {yoga}
-          </Button>
-        ))}
-    </ButtonGroup>
-    <div className="col text-center">
-      <p>{myYoga === "Ensemble des cours" && <Cours/>}</p>
-      <p>{myYoga === "Yoga Doux" && <YogaDoux/>}</p>
-      <p>{myYoga === "Yoga Dynamique" && <YogaDyn/>}</p>
-    </div>
-  </Col>
-</Row>
-)}
+                {yogas.map(yoga => (
+                  <Button variant="secondary" 
+                  style={{margin : "10px", padding : "15px"}}
+                  key={yoga}
+                  onClick={()=> setMyYoga(yoga)}
+                  >
+                    {yoga}
+                  </Button>
+                ))}
+          </ButtonGroup>
+          </Jumbotron>
+            <div className="text-center" >
+              <p>{myYoga === "Ensemble des cours" && <Lessons/>}</p>
+              <p>{myYoga === "Yoga Doux" && <YogaDoux/>}</p>
+              <p>{myYoga === "Yoga Dynamique" && <YogaDyn/>}</p>
+            </div>
+      
+        </Col>
+      </Row>
+    </Container>
+  )
+}
 export default Toggle;
